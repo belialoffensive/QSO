3c09a2dbfb6d5128f72a7a9392e5b1f38a357500
kaidenvlr <rllntmt@gmail.com>
2023-01-13 03:32:08 +0600

    COMMIT

--- demonewslist.php
+++ demonewslist.php
@@ -10,34 +10,30 @@
 
 <body id="top">
     <?php include('./partials/header.php'); ?>
-    <div class="wrapper row3">
+        <div class="wrapper row3">
         <main class="container clear">
             <div class="content">
                 <!-- ################################################################################################ -->
                 <div id="gallery">
                     <figure>
-                        <header class="heading" align="center">НОВОСТИ
+                        <header class="heading" align="center">СПИСОК НОВОСТЕЙ
                         </header>
                         <ul align="center" class="nospace clear">
                             <li class="one_quarter first"><a href="demonews.php"><img
                                         src="../images/demo/gallery/01.png" alt=""></a>
-                                <h1><a href="demonews.php">название новости 1</a></h1>
-                                <p>кратко о новости 1</p>
+                                <p><a href="demonews.php">название новости 1</a></p>
                             </li>
                             <li class="one_quarter"><a href="demonews.php"><img
                                         src="../images/demo/gallery/01.png" alt=""></a>
-                                <h1><a href="demonews.php">название новости 2</a></h1>
-                                <p>кратко о новости 2</p>
+                                <p><a href="demonews.php">название новости 2</a></p>
                             </li>
                             <li class="one_quarter"><a href="demonews.php"><img
                                         src="../images/demo/gallery/01.png" alt=""></a>
-                                <h1><a href="demonews.php">название новости 3</a></h1>
-                                <p>кратко о новости 3</p>
+                                <p><a href="demonews.php">название новости 3</a></p>
                             </li>
                             <li class="one_quarter"><a href="demonews.php"><img
                                         src="../images/demo/gallery/01.png" alt=""></a>
-                                <h1><a href="demonews.php">название новости 4</a></h1>
-                                <p>кратко о новости 4</p>
+                                <p><a href="demonews.php">название новости 4</a></p>
                             </li>
                         </ul>
                     </figure>
@@ -47,6 +43,7 @@
             <div class="clear"></div>
         </main>
     </div>
+
     <?php include('./partials/footer.php'); ?>
 
     <a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
