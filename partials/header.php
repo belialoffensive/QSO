0db0d9b3c6fef66d76b191a1aa2f72d123991de6
kaidenvlr <rllntmt@gmail.com>
2023-01-19 17:12:35 +0600

    commit

--- partials/header.php
+++ partials/header.php
@@ -53,7 +53,7 @@
     </header>
 </div>
 
-<div class="wrapper row2 bgded" style="background-image:url('images/demo/backgrounds/back2.jpg');">
+<div class="wrapper row2 bgded" style="background-image:url('../images/demo/backgrounds/back2.jpg');">
     <div class="overlay">
         <section id="pageintro" class="clear">
             <div class="center btmspace-80">
